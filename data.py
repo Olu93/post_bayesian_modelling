eec4c6f3dfe0818f46403bf233fe1c1d67dae265
Olubuntu <o.hundogan@gmail.com>
2021-11-11 15:47:56 +0100

    --amend

--- data.py
+++ data.py
@@ -16,11 +16,22 @@ def true_function_linear(x, p1=1):
     return (3 * (x**p1)) - 1
 
 
+def true_function_sigmoid(x, y, w1, w2):
+    return 1 / (1 + np.exp(-(w1 * x + w2 * y)))
+
+
 def observed_data(d: int = 10, p1=2, p2=3):
     data = np.random.randn(d, 2) * 3
     x, y = data[:, 0], data[:, 1]
     variance = 2.5
     return x, y, true_function(x, y, p1, p2) + np.random.randn(d) * variance
+
+
+def observed_data_wobbly(d: int = 10):
+    data = np.random.randn(d, 2) * 1
+    x, y = data[:, 0], data[:, 1]
+    variance = 2.5
+    return x, y, ((np.sin(x) * 5) / np.exp(y)) + np.random.randn(d) * variance
 
 
 def observed_data_linear(d: int = 10, p1=1):
@@ -29,6 +40,13 @@ def observed_data_linear(d: int = 10, p1=1):
     return x, true_function_linear(x, p1) + np.random.randn(d)
 
 
+def observed_data_binary(d: int = 10, w1=2, w2=2, std=3):
+    data = np.random.randn(d, 2) * std
+    x, y = data[:, 0], data[:, 1]
+    return x, y, true_function_sigmoid(x, y, w1, w2) > 0.5
+
+
+# %%
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 
@@ -45,3 +63,35 @@ ax.set_zlabel('Z Label')
 
 plt.show()
 # %%
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+
+n = 50
+
+# For each set of style and range settings, plot n random points in the box
+# defined by x in [23, 32], y in [0, 100], z in [zlow, zhigh].
+xs, ys, zs = observed_data_wobbly(n)
+ax.scatter(xs, ys, zs)
+
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+
+plt.show()
+
+# %%
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+
+n = 50
+
+# For each set of style and range settings, plot n random points in the box
+# defined by x in [23, 32], y in [0, 100], z in [zlow, zhigh].
+xs, ys, zs = observed_data_binary(n)
+ax.scatter(xs, ys, zs)
+
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+
+plt.show()

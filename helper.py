ba5596745667008de7c8ea3581fa0c5a026ae553
Olusanmi Hundogan <o.hundogan@gmail.com>
2021-11-15 22:59:58 +0100

    Impl: Added some helper functions

--- helper.py
+++ helper.py
@@ -1,14 +1,16 @@
 import numpy as np
 from scipy import special
 
-
 STABILITY_CONSTANT = 0.00000000001
 
+
 def log_stable(x):
-    return np.log(x+STABILITY_CONSTANT)
+    return np.log(x + STABILITY_CONSTANT)
+
 
 def sigmoid(x):
     return special.expit(x)
+
 
 def add_bias_vector(X):
     return np.hstack([np.ones(X.shape[0]).reshape(-1, 1), X])
@@ -16,3 +18,17 @@ def add_bias_vector(X):
 
 def create_polinomial_bases(X, order=3):
     return np.hstack([X**i for i in range(1, order + 1)])
+
+
+def predict(ws, X):
+    logits = X @ ws.T
+    probabilities = sigmoid(logits).mean(axis=1)
+    return probabilities[:, None]
+
+
+def compute_metrics(w, X, y):
+    y_hat = sigmoid(X @ w)
+    losses = y - y_hat
+    m_loss = np.mean(np.abs(losses))
+    m_acc = np.mean(y == ((y_hat >= 0.5) * 1.0))
+    return m_loss, m_acc

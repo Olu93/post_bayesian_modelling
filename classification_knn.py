ef109c2c55647ed5236e64dab17fd8556cc4eff8
Olusanmi Hundogan <o.hundogan@gmail.com>
2021-11-22 03:15:24 +0100

    Impl: Linear kernel knn

--- classification_knn.py
+++ classification_knn.py
@@ -1,4 +1,3 @@
-
 # %%
 import abc
 import matplotlib.pyplot as plt
@@ -16,7 +15,8 @@ from viz import plot_countours_and_points, plot_train_val_curve, plot_w_samples
 from sklearn.model_selection import train_test_split
 from collections import Counter
 from sklearn import metrics
-
+from sklearn import model_selection
+from tqdm.notebook import tqdm
 # %%
 np.set_printoptions(linewidth=100, formatter=dict(float=lambda x: "%.3g" % x))
 IS_EXACT_FORMULA = True
@@ -44,20 +44,44 @@ train_X, val_X, train_y, val_y = train_test_split(true_X,
 ax = plt.gca()
 plot_countours_and_points(ax, train_X, train_y, X_means, X_covs)
 plt.show()
+
+
 # %%
-def knn(k, X_new, train_X, train_y):
+def linear_kernel(X_sq_differences):
+    pass
+
+def knn_linear_kernel(k, X_new, train_X, train_y, kernel_func=None):
     X_new = X_new[:, None, :]
     X_differences = X_new - train_X
-    X_sq_differences = X_differences ** 2
+    X_sq_differences = X_differences**2
     X_sum_sq_differences = X_sq_differences.sum(axis=-1)
     X_root_sum_sq_differences = np.sqrt(X_sum_sq_differences)
     X_with_proximity_indices = X_root_sum_sq_differences.argsort(axis=-1)
     X_k_closest = X_with_proximity_indices[:, :k]
     label_of_k_closest = train_y[X_k_closest]
     df_label_of_k_closest = pd.DataFrame(label_of_k_closest.squeeze())
-    voting_winners = df_label_of_k_closest.apply(lambda x: x.mode(), axis=1).iloc[:,0].values
+    voting_winners = df_label_of_k_closest.apply(lambda x: x.mode(),
+                                                 axis=1).iloc[:, 0].values
     return voting_winners
 
-pred_y = knn(3,val_X, train_X, train_y)
+
+pred_y = knn_linear_kernel(3, val_X, train_X, train_y)
 metrics.accuracy_score(val_y, pred_y)
 # %%
+k_range = range(1, 20)
+num_experiments = len(k_range)
+kf = model_selection.KFold(n_splits=num_experiments)
+train_acc = np.zeros(num_experiments)
+val_acc = np.zeros(num_experiments)
+
+for k, (train_idxs, val_idxs) in tqdm(zip(k_range, kf.split(true_X, true_y)), total=num_experiments):
+    X_train_subset, X_val_subset = true_X[train_idxs], true_X[val_idxs]
+    y_train_subset, y_val_subset = true_y[train_idxs], true_y[val_idxs]
+    y_train_pred = knn_linear_kernel(k, X_train_subset, X_train_subset, y_train_subset)
+    y_val_pred = knn_linear_kernel(k, X_val_subset, X_train_subset, y_train_subset)
+    train_acc[k - 1] = metrics.accuracy_score(y_train_subset, y_train_pred)
+    val_acc[k - 1] = metrics.accuracy_score(y_val_subset, y_val_pred)
+
+plt.plot(k_range,train_acc, label="train")
+plt.plot(k_range,val_acc, label="val")
+plt.legend()

9acac9a9d10b4e771f96cb089e3f8a3aaeaf3f66
Olusanmi Hundogan <o.hundogan@gmail.com>
2021-11-30 21:39:24 +0100

    Start: PPCA with missing vals

--- dimensionality_reduction_probablistic_principal_component_analysis.py
+++ dimensionality_reduction_probablistic_principal_component_analysis.py
@@ -213,3 +213,93 @@ for cls in data_y.unique():
     ax2.view_init(30, 45)
 plt.show()
 Axes3D
+# %%
+def compute_sigma_x_Z(N, exp_tau, exp_cov_w, Z):
+    # exp_tau:      1x1
+    # exp_cov_w:    MxDxD
+    D = exp_cov_w.shape[1]
+    # DxD
+    I_D = np.eye(D)
+
+    # DxD
+    new_sigma_x = np.linalg.inv(I_D + exp_tau * exp_cov_w.sum(axis=(0)))
+    # NxDxD
+    all_sigmas = np.repeat(new_sigma_x[None], N, axis=0)
+    # Out:          NxDxD
+    return all_sigmas
+
+
+# Σ_µm
+def compute_sigma_w_Z(M, exp_tau, exp_cov_x, Z):
+    # exp_tau:      1x1
+    # exp_cov_x:    NxDxD
+    D = exp_cov_x.shape[1]
+    # DxD
+    I_D = np.eye(D)
+
+    # DxD
+    new_sigma_w = np.linalg.inv(I_D + exp_tau * exp_cov_x.sum(axis=0))
+
+    # MxDxD
+    all_sigmas = np.repeat(new_sigma_w[None], M, axis=0)
+    # Out:          MxDxD
+    return all_sigmas
+
+
+def compute_mu_x_Z(Y, exp_tau, cov_x_N, exp_w_M, Z):
+    # Y: N x M
+    # exp_tau = 1 x 1
+    # exp_w_M: M x D
+    # sigma_x_N: N x D x D
+
+    # NxD
+    # -- Summation over M is handled by @-multiplication
+    YxW = (Y @ exp_w_M)
+    # NxD => 1x1 * NxDxD * NxDx1
+    result = exp_tau * np.einsum('ijk,ij->ik', cov_x_N, YxW)
+    return result
+
+
+def compute_mu_w_Z(Y, exp_tau, cov_w_M, exp_x_N, Z):
+    # Y: N x M
+    # exp_tau = 1 x 1
+    # exp_w_M: N x D
+    # cov_x_n: M x D x D
+
+    # MxD
+    # -- Summation over M is handled by @-multiplication
+    YxX = (Y.T @ exp_x_N)
+    # MxD => 1x1 * DxD @ MxDx1
+    result = exp_tau * np.einsum('ijk,ij->ik', cov_w_M, YxX)
+    return result
+
+
+def probablistic_principal_component_analysis_w_missing_values(Y, dim=3, a=1, b=1):
+    N, M = Y.shape  # N-observed. M-dimensional input vectors y_n
+    D = dim
+    I_D = np.eye(D)
+    exp_tau = a / b
+    e, f = a, b
+    # MxD
+    exp_w = np.random.multivariate_normal(np.zeros(D), I_D, size=M)
+    # MxDxD
+    exp_ww = I_D + np.einsum('ijk,ikj->ijk', exp_w[:, None], exp_w[:, None])
+
+    for i in range(2):
+        sigma_x = compute_sigma_x(N, exp_tau, exp_ww)
+        mu_x = compute_mu_x(Y, exp_tau, sigma_x, exp_w)
+
+        exp_x = mu_x
+        exp_xx = compute_exp_cov_x(sigma_x, mu_x)
+
+        sigma_w = compute_sigma_w(M, exp_tau, exp_xx)
+        mu_w = compute_mu_w(Y, exp_tau, sigma_w, exp_x)
+
+        exp_w = mu_w
+        exp_ww = compute_exp_cov_w(sigma_w, mu_w)
+
+        last_part = compute_final_exp(exp_ww, exp_xx, exp_x)
+        e, f = compute_exp_tau(e, f, Y, exp_w, exp_x, last_part)
+        exp_tau = e / f
+
+    return exp_w, pd.DataFrame(exp_x)
